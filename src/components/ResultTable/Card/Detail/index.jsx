b630fb6410566bca8d68681819362ed24584fa25
xizh1027661 <xizh1027661@stu.sjcc.edu>
2024-06-11 19:03:00 -0700

    adding rating number to card and detail

--- src/components/ResultTable/Card/Detail/index.jsx
+++ src/components/ResultTable/Card/Detail/index.jsx
@@ -1,11 +1,11 @@
 import { Rating } from "../../../Filter/RatingFilter/Rating"
 import './Detail.css'
 
-function Detail ({title, price }) {
+function Detail ({title, price, rating}) {
     return (
         <div className="DetailsCardContainer">
             <h3>{title}</h3>
-            <Rating stars={3}/>
+            <Rating stars={Math.round(rating.rate)}/>
             <h3>${price}</h3>
         </div>
     )
