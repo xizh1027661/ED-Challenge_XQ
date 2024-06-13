06b8ff653865dd965b26a118228d4693130bbdb2
xizh1027661 <xizh1027661@stu.sjcc.edu>
2024-06-12 18:37:23 -0700

    filtrer work once

--- src/components/ResultInfoBar/SortFilter/index.jsx
+++ src/components/ResultInfoBar/SortFilter/index.jsx
@@ -1,6 +1,12 @@
 import './SortFilter.css'
+import { useContext } from 'react';
+import { SearchContext } from '../../../contexts/SearchContext';
 
 function SortFilter () {
+    const {
+        searchedProducts,
+    }= useContext(SearchContext);
+    searchedProducts.sort(function(a, b){return a.price - b.price});
     return (
         <div className='SortFilterContainer'>
             <select name="order" id="order">
