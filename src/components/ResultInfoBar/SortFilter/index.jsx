394ce963a9e3eeaf239cafffe36cd44594e9c1c1
xizh1027661 <xizh1027661@stu.sjcc.edu>
2024-06-14 02:25:17 -0700

    sort() works in filter function

--- src/components/ResultInfoBar/SortFilter/index.jsx
+++ src/components/ResultInfoBar/SortFilter/index.jsx
@@ -6,10 +6,21 @@ function SortFilter () {
     const {
         searchedProducts,
     }= useContext(SearchContext);
-    searchedProducts.sort(function(a, b){return a.price - b.price});
+   
+    function filterOrder(selectedOrder){
+        var order = selectedOrder;
+        var order = "Price_High"
+        //console.log(order);
+        if (order == 'Price_Low') {
+            searchedProducts.sort(function(a, b){return a.price - b.price});
+        } else if (order == 'Price_High'){
+            searchedProducts.sort(function(a, b){return b.price - a.price});
+        }      
+    }
+    
     return (
         <div className='SortFilterContainer'>
-            <select name="order" id="order">
+            <select name="order" id="order" onChange={filterOrder(this)}>
                 <option value="Name">Name</option>
                 <option value="Price_Low">Price: Low to High</option>
                 <option value="Price_High">Price: High to Low</option>
